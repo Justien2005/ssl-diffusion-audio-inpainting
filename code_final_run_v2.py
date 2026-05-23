2f9c1c9107b06abb78743be909ee902bd3a620fb
JUS/CHDEV <justien.venerdi@binus.ac.id>
2026-05-23 12:13:31 +0700

    #repreocess

--- code_final_run_v2.py
+++ code_final_run_v2.py
@@ -470,7 +470,7 @@ import time
 # ============================================================
 
 # Set True jika preprocessing sudah pernah dijalankan
-SKIP_IF_EXISTS = True
+SKIP_IF_EXISTS = False
 
 # Seed tetap untuk semua sampling dataset agar eksperimen reproducible
 DATASET_RANDOM_SEED = 42
